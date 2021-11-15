b87342e2117066e682817e280afc9cc2786a578e
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-11-15 14:38:11 +0700

    include redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
-console.log('Hello Redux');
+const incBtn = document.querySelector('#inc');
+const decBtn = document.querySelector('#dec');
+const rndBtn = document.querySelector('#rnd');
+const counterEl = document.querySelector('#counter');
+
+const initialState = { value: 0 };
+
+// reducer
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case 'INC':
+      return ({ ...state, value: state.value + 1 });
+    case 'DEC':
+      return ({ ...state, value: state.value - 1 });
+    case 'RND':
+      return ({ ...state, value: state.value * payload });
+    default:
+      return state;
+  }
+}
+
+// Store create
+const store = createStore(reducer);
+
+const updateCounter = () => {
+  counterEl.textContent = store.getState().value;
+};
+
+// Subscribe store
+store.subscribe(updateCounter);
+
+// Action Creators
+const inc = () => ({ type: 'INC' });
+const dec = () => ({ type: 'DEC' });
+const rnd = (value) => ({ type: 'INC', payload: value });
+
+incBtn.addEventListener('click', (e) => {
+  store.dispatch(inc());
+});
+
+decBtn.addEventListener('click', (e) => {
+  store.dispatch(dec());
+});
+
+rndBtn.addEventListener('click', (e) => {
+  const rndValue = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(rndValue));
+});
 
 ReactDOM.render(
   <React.StrictMode>
